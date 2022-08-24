7b91fdb9c727208fa1f8ac6b48a4ad2521f34d89
MohamedAshraf <55748253+Op262@users.noreply.github.com>
2022-08-24 04:39:13 +0200

    feat(WebSocket): added websocket and updted the configs

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ const {Websocket} = require('../src/index')
 
 const s = new WebSocket('any')
 
-s.connectClient('ODYzNDA2ODc1NjU5MDc1NjE0.GUYarQ.QVdY1DMfCu9vwNm26nDF47ViaJ1DnOwMa7sbzs')
+s.connectClient('bot-token')
