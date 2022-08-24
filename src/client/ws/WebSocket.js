c4ebf2954fd2a660a8bf145a65b5d966c69cafdb
MohamedAshraf <55748253+Op262@users.noreply.github.com>
2022-08-24 04:40:57 +0200

    feat(WebSocket): added websocket and updted the configs

---
+++ src/client/ws/WebSocket.js
@@ -0,0 +1,67 @@
+const EventEmitter = require('events');
+const WebSocket = require('ws');
+const GATEWAY = 'wss://gateway.discord.gg/?v=10&encoding=json';
+/**
+ * The base class for all clients.
+ * @extends {EventEmitter}
+ */
+class WebSocketManager extends EventEmitter {
+  socket = new WebSocket(GATEWAY);
+  ackRecieved = false;
+  interval = 0;
+  client;
+  constructor(client) {
+    super();
+    this.client = client;
+  }
+  async connectClient(token) {
+    try {
+      this.socket.on('message', async data => {
+        const payload = JSON.parse(data.toString());
+
+        if (payload.op === 10) {
+           this.interval = await this.heartbeat(payload.d.heartbeat_interval);
+          
+           await this.identify(token)
+        }else if(payload.op == 11){
+            this.ackRecieved = true
+        }else if(payload.op == 9){
+            throw Error('Invalid gateway session')
+        }
+        if(payload.t){
+            // will continue for client constructor later...
+        }
+      });
+    } catch (err) {
+      throw Error(err);
+    }
+  }
+  async heartbeat(ms) {
+    return setInterval(() => {
+      this.socket.send(
+        JSON.stringify({
+          op: 1,
+          d: null,
+        }),
+      );
+    }, ms);
+  }
+  identify(token) {
+    this.socket.send(
+      JSON.stringify({
+        op: 2,
+        d:{
+            token: token,
+            intents: 513,
+            properties: {
+              os: 'linux',
+              browser: 'lowcord.js',
+              device: 'lowcord.js',
+            },
+        }
+      }),
+    );
+  }
+}
+
+module.exports = WebSocketManager;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const Websocket = require('./client/ws/WebSocket');
+
+exports.Websocket = Websocket
